fe5d1bf86cba5b12da9be853972cabdf77f026b6
Scorpoll23 <oliverthompsonuni@gmail.com>
2023-04-17 20:36:53 +0100

    Added Elo Calulator and added more to catControl

--- src/main/java/com/example/catvote/model/CatRepository.java
+++ src/main/java/com/example/catvote/model/CatRepository.java
@@ -1,8 +1,12 @@
 package com.example.catvote.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CatRepository extends JpaRepository<Cat, Long> {
+
+    List<Cat> getRandomCatPair();
 }
