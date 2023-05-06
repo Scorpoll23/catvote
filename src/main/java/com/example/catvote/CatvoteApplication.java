3da0a49ff7a92e66ad97a1403ac15f0b4a282047
Scorpoll23 <oliverthompsonuni@gmail.com>
2023-05-06 15:44:23 +0100

    Almost done

--- src/main/java/com/example/catvote/CatvoteApplication.java
+++ src/main/java/com/example/catvote/CatvoteApplication.java
@@ -12,10 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class CatvoteApplication {
 
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(CatvoteApplication.class, args);
-
+		
 	}
-
 }
